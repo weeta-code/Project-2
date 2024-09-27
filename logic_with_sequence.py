34259fd7dfb60c1dddf422615d7bffd022e9e4d5
Victor DeSouza <victordesouza132@gmail.com>
2024-09-27 19:30:04 -0400

    Add files via upload

--- logic_with_sequence.py
+++ logic_with_sequence.py
@@ -5,14 +5,20 @@
 from logic import nand, implies, iff, xor
 
 def nand_of_tuple(tuple_nand):
-    A = tuple_nand[0]
-    B = tuple_nand[1]
+    A, B = tuple_nand[0, 1]
     return nand(A, B)
 
 def implies_in_both_directions(list_implies):
-   A = list_implies[0]
-   B = list_implies[1]
-
-   list_implies[0] = implies(A, B)
-   list_implies[1] = implies(B, A)
+   A, B = list_implies[0, 1]
+   list_implies = [implies(A, B), implies(B, A)]
    return list_implies
+
+def iff_with_result(list_append):
+    A, B = list_append[0, 1]
+    list_append = list_append.append(iff(A, B))
+    return list_append
+
+def xor_with_result_in_between(list_xor):
+    A, B = list_xor[0, 1]
+    list_xor = list_xor.insert(xor(A, B), 1)
+    return list_xor
